f7492e1afa5c68e8718c9cbbfd80177aa92e4186
otakisan <takashi@aquamarine.local>
2015-02-22 14:57:41 +0900

    商品情報をクロール

--- MyPocketSBUX/lib/tasks/food_list_crawler.rb
+++ MyPocketSBUX/lib/tasks/food_list_crawler.rb
@@ -1,6 +1,7 @@
 require 'open-uri'
 require 'nokogiri'
 
+ROOT_URL = "http://www.starbucks.co.jp"
 
 def traceoutproduct(node)
 
@@ -14,6 +15,22 @@ def traceoutproduct(node)
   p node.css('a').attribute('href').value
 end
 
+def product(node)
+  
+  product_url = node.css('a').attribute('href').value
+  product_doc = Nokogiri::HTML(open(ROOT_URL + product_url))
+  #p product_doc.title 
+
+  product_detail = product_doc.xpath('//article[contains(@class, "productDetail")]')
+  product_name = product_detail.css('h2').inner_text
+  product_price = product_detail.xpath('//p[@class="productInfoDetail"]//span[@class="price"]').inner_text
+  p product_name
+  p product_price
+
+  product_special = product_detail.xpath('//div[@class="productInfo"]//p[@class="specialItem"]/span').inner_text
+  p product_special
+end
+
 doc = Nokogiri::HTML(open("http://www.starbucks.co.jp/food/"))
 
 p doc.title
@@ -21,12 +38,14 @@ doc.xpath('//article[contains(@class,"productList")]').each do |node|
 
   p "オススメ"
   node.xpath('//li[@class="col recommend"]').each do |recommended|
-    traceoutproduct(recommended)
+    #traceoutproduct(recommended)
+    #product(recommended)
   end
 
   p "レギュラー"
   node.xpath('//li[@class="col "]').each do |regular|
-    traceoutproduct(regular)
+    #traceoutproduct(regular)
+    product(regular)
   end
 
 end
