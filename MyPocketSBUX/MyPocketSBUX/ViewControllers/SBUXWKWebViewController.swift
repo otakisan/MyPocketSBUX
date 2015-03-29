07d0eea7afbd71fa83eefc32c208df32b47eb42b
otakisan <takashi@aquamarine.local>
2015-03-29 19:59:33 +0900

    プレスリリースの公式サイト読み込み時に、ウェイティングサークルを表示

--- MyPocketSBUX/MyPocketSBUX/ViewControllers/SBUXWKWebViewController.swift
+++ MyPocketSBUX/MyPocketSBUX/ViewControllers/SBUXWKWebViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 import WebKit
 
-class SBUXWKWebViewController: UIViewController {
+class SBUXWKWebViewController: UIViewController, WKNavigationDelegate {
 
+    //  デバイスのCGRectを取得
+    var deviceBound : CGRect = UIScreen.mainScreen().bounds
+    
     var relativePath = ""
     var webkitview : WKWebView = WKWebView()
+    var activityIndicatorView : UIActivityIndicatorView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,9 +25,12 @@ class SBUXWKWebViewController: UIViewController {
     }
     
     func initializeWebView(){
-        self.view = self.webkitview
+        self.webkitview.frame = CGRectMake(0, 0/*60 上に余白が欲しいとき*/, deviceBound.size.width, deviceBound.size.height - 60)
+        self.view.addSubview(self.webkitview)
+        self.webkitview.navigationDelegate = self
         
         if var url = NSURL(string:"http://www.starbucks.co.jp/\(self.relativePath)"){
+            self.showActivityIndicator()
             var req = NSURLRequest(URL:url)
             self.webkitview.loadRequest(req)
         }
@@ -34,6 +41,28 @@ class SBUXWKWebViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func showActivityIndicator() {
+        
+        if self.activityIndicatorView == nil {
+            var activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.Gray)
+            activityIndicator.hidesWhenStopped = true
+//            var indicatorSize = activityIndicator.frame.size
+//            var viewSize = self.view.frame.size
+//            var left = (viewSize.width - indicatorSize.width) / 2
+//            var top = (viewSize.height - indicatorSize.height) / 2
+//            activityIndicator.frame = CGRectMake(left, top, indicatorSize.width, indicatorSize.height)
+            activityIndicator.center = self.view.center
+            self.view.addSubview(activityIndicator)
+            
+            self.activityIndicatorView = activityIndicator
+        }
+        
+        self.activityIndicatorView?.startAnimating()
+    }
+    
+    func webView(webView: WKWebView, didFinishNavigation navigation: WKNavigation!){
+        self.activityIndicatorView?.stopAnimating()
+    }
 
     /*
     // MARK: - Navigation
