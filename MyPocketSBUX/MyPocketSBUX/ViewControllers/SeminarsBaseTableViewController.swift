6630935207d0eb16979bf3a8b367037843e0b808
Takashi Ikeda <takashi@scorpius.local>
2015-04-04 07:26:24 +0900

    セミナー一覧に検索コントロールを追加

---
+++ MyPocketSBUX/MyPocketSBUX/ViewControllers/SeminarsBaseTableViewController.swift
@@ -0,0 +1,127 @@
+//
+//  SeminarsBaseTableViewController.swift
+//  MyPocketSBUX
+//
+//  Created by Takashi Ikeda on 2015/04/02.
+//  Copyright (c) 2015年 Takashi Ikeda. All rights reserved.
+//
+
+import UIKit
+
+class SeminarsBaseTableViewController: UITableViewController {
+
+    struct Constants {
+        struct Nib {
+            static let name = "TableCell"
+        }
+        
+        struct TableViewCell {
+            static let identifier = "defaultSeminarTableViewCellIdentifier"
+        }
+    }
+
+    let statusMappings = ["1" : "available", "2" : "almost full", "3" : "full"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func configureCell(cell: UITableViewCell, forSeminars seminars: [[NSDictionary]], indexPath : NSIndexPath) {
+        let objDic = seminars[indexPath.section][indexPath.row]
+        let storeName = (objDic["store"] as NSDictionary)["name"] as NSString
+        let capacity = objDic["capacity"] as Int
+        let status = objDic["status"] as String
+        let start_time = objDic["start_time"] as String
+        
+        if let startDateTime = DateUtility.dateFromSqliteDateString(start_time) {
+            let startDate = DateUtility.localDateString(startDateTime)
+            let startTime = DateUtility.localTimeString(startDateTime)
+            let capacityLabel = "capacity".localized()
+            let statusLabel = "status".localized()
+            let statusValue = (self.statusMappings[status] == nil ? "" : self.statusMappings[status]!).localized()
+            cell.detailTextLabel?.text = "\(startDate) \(startTime)  \(capacityLabel):\(capacity)  \(statusLabel):\(statusValue)"
+        }
+        cell.textLabel?.text = "\(storeName)"
+    }
+
+    // MARK: - Table view data source
+
+//    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+//        // #warning Potentially incomplete method implementation.
+//        // Return the number of sections.
+//        return 0
+//    }
+//
+//    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // #warning Incomplete method implementation.
+//        // Return the number of rows in the section.
+//        return 0
+//    }
+
+    /*
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MyPocketSBUX/MyPocketSBUX/ViewControllers/SeminarsTableViewController.swift
+++ MyPocketSBUX/MyPocketSBUX/ViewControllers/SeminarsTableViewController.swift
@@ -8,12 +8,17 @@
 
 import UIKit
 
-class SeminarsTableViewController: UITableViewController {
+class SeminarsTableViewController: SeminarsBaseTableViewController, UISearchBarDelegate, UISearchControllerDelegate, UISearchResultsUpdating {
     
     var seminarData : [[NSDictionary]]?
     var groupKeyToIndex : [String:Int] = [:]
-    let statusMappings = ["1" : "available", "2" : "almost full", "3" : "full"]
     //var seminarEditions : [String:NSDictionary]?
+    
+    // Search controller to help us with filtering.
+    var searchController: UISearchController!
+    
+    // Secondary search results table view.
+    var filteredSeminarsTableController: FilteredSeminarsTableViewController!
 
     func initializeData(){
         
@@ -81,6 +86,79 @@ class SeminarsTableViewController: UITableViewController {
         return "http://localhost:3000/seminars.json"
     }
     
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        /*
+        // Update the filtered array based on the search text.
+        let searchResults = products
+        
+        // Strip out all the leading and trailing spaces.
+        let whitespaceCharacterSet = NSCharacterSet.whitespaceCharacterSet()
+        let strippedString = searchController.searchBar.text.stringByTrimmingCharactersInSet(whitespaceCharacterSet)
+        let searchItems = strippedString.componentsSeparatedByString(" ") as [String]
+        
+        // Build all the "AND" expressions for each value in the searchString.
+        var andMatchPredicates = [NSPredicate]()
+        
+        for searchString in searchItems {
+            // Each searchString creates an OR predicate for: name, yearIntroduced, introPrice.
+            //
+            // Example if searchItems contains "iphone 599 2007":
+            //      name CONTAINS[c] "iphone"
+            //      name CONTAINS[c] "599", yearIntroduced ==[c] 599, introPrice ==[c] 599
+            //      name CONTAINS[c] "2007", yearIntroduced ==[c] 2007, introPrice ==[c] 2007
+            //
+            var searchItemsPredicate = [NSPredicate]()
+            
+            // Below we use NSExpression represent expressions in our predicates.
+            // NSPredicate is mmiade up of smaller, atomic parts: two NSExpressions (a left-hand value and a right-hand value).
+            
+            // Name field matching.
+            var lhs = NSExpression(forKeyPath: "title")
+            var rhs = NSExpression(forConstantValue: searchString)
+            
+            var finalPredicate = NSComparisonPredicate(leftExpression: lhs, rightExpression: rhs, modifier: .DirectPredicateModifier, type: .ContainsPredicateOperatorType, options: .CaseInsensitivePredicateOption)
+            
+            searchItemsPredicate.append(finalPredicate)
+            
+            let numberFormatter = NSNumberFormatter()
+            numberFormatter.numberStyle = .NoStyle
+            numberFormatter.formatterBehavior = .BehaviorDefault
+            
+            let targetNumber = numberFormatter.numberFromString(searchString)
+            // `searchString` may fail to convert to a number.
+            if targetNumber != nil {
+                // `yearIntroduced` field matching.
+                lhs = NSExpression(forKeyPath: "yearIntroduced")
+                rhs = NSExpression(forConstantValue: targetNumber!)
+                finalPredicate = NSComparisonPredicate( leftExpression: lhs, rightExpression: rhs, modifier: .DirectPredicateModifier, type: .EqualToPredicateOperatorType, options: .CaseInsensitivePredicateOption)
+                
+                searchItemsPredicate.append(finalPredicate)
+                
+                // `price` field matching.
+                lhs = NSExpression(forKeyPath: "introPrice")
+                rhs = NSExpression(forConstantValue: targetNumber!)
+                finalPredicate = NSComparisonPredicate( leftExpression: lhs, rightExpression: rhs, modifier: .DirectPredicateModifier, type: .EqualToPredicateOperatorType, options: .CaseInsensitivePredicateOption)
+                
+                searchItemsPredicate.append(finalPredicate)
+            }
+            
+            // Add this OR predicate to our master AND predicate.
+            let orMatchPredicates = NSCompoundPredicate.orPredicateWithSubpredicates(searchItemsPredicate)
+            andMatchPredicates.append(orMatchPredicates)
+        }
+        
+        // Match up the fields of the Product object.
+        let finalCompoundPredicate = NSCompoundPredicate.andPredicateWithSubpredicates(andMatchPredicates)
+        
+        let filteredResults = searchResults.filter { finalCompoundPredicate.evaluateWithObject($0) }
+        
+        // Hand over the filtered results to our search results table.
+        let resultsController = searchController.searchResultsController as ResultsTableController
+        resultsController.filteredProducts = filteredResults
+        resultsController.tableView.reloadData()
+*/
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -89,6 +167,27 @@ class SeminarsTableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        // サーチ後の結果
+        self.filteredSeminarsTableController = FilteredSeminarsTableViewController()
+        
+        // We want to be the delegate for our filtered table so didSelectRowAtIndexPath(_:) is called for both tables.
+        self.filteredSeminarsTableController.tableView.delegate = self
+        
+        self.searchController = UISearchController(searchResultsController: self.filteredSeminarsTableController)
+        self.searchController.searchResultsUpdater = self
+        self.searchController.searchBar.sizeToFit()
+        self.tableView.tableHeaderView = searchController.searchBar
+        
+        self.searchController.delegate = self
+        self.searchController.dimsBackgroundDuringPresentation = false // default is YES
+        self.searchController.searchBar.delegate = self    // so we can monitor text changes + others
+        
+        // Search is now just presenting a view controller. As such, normal view controller
+        // presentation semantics apply. Namely that presentation will walk up the view controller
+        // hierarchy until it finds the root view controller or one that defines a presentation context.
+        self.definesPresentationContext = true
+
         
         self.initializeData()
     }
@@ -113,26 +212,28 @@ class SeminarsTableViewController: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("defaultSeminarTableViewCellIdentifier", forIndexPath: indexPath) as UITableViewCell
+        let cell = tableView.dequeueReusableCellWithIdentifier(Constants.TableViewCell.identifier, forIndexPath: indexPath) as UITableViewCell
 
         // Configure the cell...
         if let tableData = self.seminarData {
-            // エディションでセクション分けする
-            let objDic = tableData[indexPath.section][indexPath.row]
-            let storeName = (objDic["store"] as NSDictionary)["name"] as NSString
-            let capacity = objDic["capacity"] as Int
-            let status = objDic["status"] as String
-            let start_time = objDic["start_time"] as String
+            self.configureCell(cell, forSeminars: tableData, indexPath: indexPath)
             
-            if let startDateTime = DateUtility.dateFromSqliteDateString(start_time) {
-                let startDate = DateUtility.localDateString(startDateTime)
-                let startTime = DateUtility.localTimeString(startDateTime)
-                let capacityLabel = "capacity".localized()
-                let statusLabel = "status".localized()
-                let statusValue = (self.statusMappings[status] == nil ? "" : self.statusMappings[status]!).localized()
-                cell.detailTextLabel?.text = "\(startDate) \(startTime)  \(capacityLabel):\(capacity)  \(statusLabel):\(statusValue)"
-            }
-            cell.textLabel?.text = "\(storeName)"
+            // エディションでセクション分けする
+//            let objDic = tableData[indexPath.section][indexPath.row]
+//            let storeName = (objDic["store"] as NSDictionary)["name"] as NSString
+//            let capacity = objDic["capacity"] as Int
+//            let status = objDic["status"] as String
+//            let start_time = objDic["start_time"] as String
+//            
+//            if let startDateTime = DateUtility.dateFromSqliteDateString(start_time) {
+//                let startDate = DateUtility.localDateString(startDateTime)
+//                let startTime = DateUtility.localTimeString(startDateTime)
+//                let capacityLabel = "capacity".localized()
+//                let statusLabel = "status".localized()
+//                let statusValue = (self.statusMappings[status] == nil ? "" : self.statusMappings[status]!).localized()
+//                cell.detailTextLabel?.text = "\(startDate) \(startTime)  \(capacityLabel):\(capacity)  \(statusLabel):\(statusValue)"
+//            }
+//            cell.textLabel?.text = "\(storeName)"
         }
 
         return cell
